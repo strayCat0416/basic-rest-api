a4e04615b36b188a99144e69fc1bf13448f06e16
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-01 21:52:08 +0900

    User情報を全取得するAPI

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3');
+const dbPath = 'app/db/database.sqlite3';
 
-app.get('/api/v1/hello', (req, res) => {
-  res.json({message: 'Hello, World!'});
+//Get all users
+app.get('/api/v1/users', (req, res) => {
+  //Connect database
+  const db = new sqlite3.Database(dbPath);
+  db.all('SELECT * FROM users', (err, rows) => {
+    res.json(rows);
+  });
+  db.close();
 });
 
 const port = process.env.PORT || 3000;
