f8a512dba8e557e5b21afa8dbe07992bf73340f1
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-01 22:32:21 +0900

    リクエストの作成

--- app/app.js
+++ app/app.js
@@ -7,8 +7,31 @@ const dbPath = 'app/db/database.sqlite3';
 app.get('/api/v1/users', (req, res) => {
   //Connect database
   const db = new sqlite3.Database(dbPath);
-  db.all('SELECT * FROM users', (err, rows) => {
+  db.all(`SELECT * FROM users`, (err, rows) => {
     res.json(rows);
+  });
+  db.close();
+});
+
+//Search users matching keyword
+app.get('/api/v1/search', (req, res) => {
+  //Connect database
+  const db = new sqlite3.Database(dbPath);
+  const keyword = req.query.q;
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+    res.json(rows);
+  });
+  db.close();
+});
+
+//Get a user
+app.get('/api/v1/users/:id', (req, res) => {
+  //Connect database
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    res.json(row);
   });
   db.close();
 });
