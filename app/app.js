67a69809bb73d8c327b6a214be6c13fe575ba163
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-03 16:45:55 +0900

    PUTメソッドで実行するAPIを作成

--- app/app.js
+++ app/app.js
@@ -47,6 +47,20 @@ app.get('/api/v1/users/:id', (req, res) => {
   db.close();
 });
 
+const run = async (sql, db, res, message) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, err => {
+      if (err) {
+        res.status(500).send(err);
+        return reject();
+      } else {
+        res.json({message: message});
+        return resolve();
+      }
+    });
+  });
+};
+
 //Create a new user
 app.post('/api/v1/users', async (req, res) => {
   //Connect database
@@ -55,23 +69,36 @@ app.post('/api/v1/users', async (req, res) => {
   const profile = req.body.profile ? req.body.profile : '';
   const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : '';
 
-  const run = async sql => {
-    return new Promise((resolve, reject) => {
-      db.run(sql, err => {
-        if (err) {
-          res.status(500).send(err);
-          return reject();
-        } else {
-          res.json({message: '新規ユーザーを作成しました！'});
-          return resolve();
-        }
-      });
-    });
-  };
-
   await run(
     `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}","${profile}","${dateOfBirth}")`,
+    db,
+    res,
+    '新規ユーザーを作成しました！',
   );
+  db.close();
+});
+
+//Update user data
+app.put('/api/v1/users/:id', async (req, res) => {
+  //Connect database
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
+
+  //現在のユーザー情報を取得する
+  db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+    //リクエストのbodyに値が存在していたら（入力値があれば）値を更新。なければ既存のデータのまま
+    const name = req.body.name ? req.body.name : row.name;
+    const profile = req.body.profile ? req.body.profile : row.profile;
+    const dateOfBirth = req.body.date_of_birth
+      ? req.body.date_of_birth
+      : row.date_of_birth;
+    await run(
+      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+      db,
+      res,
+      'ユーザー情報を更新しました！',
+    );
+  });
   db.close();
 });
 
