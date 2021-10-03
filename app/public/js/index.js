ddb7b04b46fa64f11f1fe463eec4a56dcefb4e63
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-03 22:56:34 +0900

    クリックイベントの設定

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,7 +1,24 @@
 const indexModule = (() => {
-  document.getElementById('search-btn').addEventListener('click', () => {
-    return searchModule.searchUsers();
-  });
+  const path = window.location.pathname;
 
-  return usersModule.fetchAllUsers();
+  switch (path) {
+    case '/':
+      document.getElementById('search-btn').addEventListener('click', () => {
+        return searchModule.searchUsers();
+      });
+
+      return usersModule.fetchAllUsers();
+
+    case '/create.html':
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.createUser();
+      });
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return (window.location.href = '/');
+      });
+      break;
+
+    default:
+      break;
+  }
 })();
