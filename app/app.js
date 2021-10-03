43dd59ec9044541736e9a34740596a382876c903
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-03 14:03:24 +0900

    リクエストのbodyをパースする設定を追加

--- app/app.js
+++ app/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const dbPath = 'app/db/database.sqlite3';
 
+//リクエストのbodyをパースする設定
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//publicディレクトリを静的ファイル群のルートディレクトリとして設定
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Get all users
