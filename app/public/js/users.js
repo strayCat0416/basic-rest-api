a7cac3e7e3f72e8e471a003f3b6df3b6196dfcdb
strayCat0416 <kyoto.masayuki@gmail.com>
2021-10-06 11:14:36 +0900

    メニューボタンの追加

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -17,6 +17,7 @@ const usersModule = (() => {
         <td>${user.date_of_birth}</td>
         <td>${user.created_at}</td>
         <td>${user.updated_at}</td>
+        <td><a href="edit.html?uid=${user.id}">編集</a></td>
          </tr>`;
         document
           .getElementById('users-list')
